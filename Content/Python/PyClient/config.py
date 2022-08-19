13d93fa4fb9e157288b8d0b1f6aad5bfc1c87e38
den <den@id.com>
2022-08-19 11:46:19 +0700

    up2

---
+++ Content/Python/PyClient/config.py
@@ -0,0 +1,46 @@
+
+# File System Paths
+
+# Assets Main Drive
+source_drive = 'L:/'
+# Drive to find shot build files
+work_drive = 'W:/'
+# Projects MEME json directory
+meme_shows_path = 'M:/SHOWS'
+#movie pipeline preset path
+mp_presets = '/Game/Cinematics/MoviePipeline/Presets'
+
+# meme folder
+meme = 'M:/SCRIPTS_WHK'
+
+# MEME Specifics
+
+# Some projects Like WHM are host of different sub-projects as episode. to specify assets of
+# different projects we use some suffixes that do not follow a resolvable address
+episode_code_suffix = {'EPWHR': '_AR_', 'EPWHH':'_HH_', 'EPWHC':'_CV_'}
+
+# UNREAL Specifics
+#Lightsets path
+lightset_path = '/Game/ASSETS/LIGHTING/'
+#Setdress path
+setdress_path = '/Game/ASSETS/SETDRESS/'
+# shots address
+shots_path = '/Game/SHOTS'
+# Template shot address
+sh0000_path = shots_path + '/SH0000'
+
+#address of maya export
+may2019a_py_exe = '\"C:/Program Files/Autodesk/Maya2019/bin/mayapy.exe\"'
+
+#address of meme python
+meme_python = 'M:/SCRIPTS/PACKAGES/python27/python.exe'
+
+# turntable folder
+turntable_folder = '/Game/TURNTABLES'
+
+
+# project preset for new settings file generation
+project_preset = 'WHM'
+
+#Assets rigged
+rigged_assets = ['CHAR', 'PROP', 'VECL']
